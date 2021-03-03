523c871240aa8f9a6ceb364bb0980dd41bf93c4b
Caio Queiroz <caioqueirozn@hotmail.com>
2021-03-03 14:36:27 -0300

    Criando projeto

---
+++ src/test/java/com/queirozcaio/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queirozcaio.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
